fdaf7abf20faa72513ab8b7cfd2e27f23a69826d
qexat <43090614+qexat@users.noreply.github.com>
2023-03-10 20:16:23 +0100

    Add download script (#67)

---
+++ download.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Clone the main branch and install Vism.
+
+perror() {
+	printf '\x1b[1;31m❌️ %s\x1b[m\n' "$1" >&2
+}
+
+throw_error() {
+	perror "$1"
+	exit "$2"
+}
+
+timed_clone() {
+	timeout 10 git clone "$1"
+}
+
+if ! (timed_clone "git@github.com:qexat/PyVism.git" || timed_clone "https://github.com/qexat/PyVism.git"); then
+	throw_error "Could not fetch the repository" 1
+fi
+
+cd ./PyVism/ || throw_error "An unknown error occured" 1
+
+. ./install.sh
+. ./utils.sh
+
+pnewline
+pinfo "Type '. $VIRTUAL_ENV/bin/activate' to activate the virtual environment"
