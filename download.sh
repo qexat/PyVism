f37af4419ee4809d1c9eaace9fb98fe9f36531a6
Qexat <43090614+qexat@users.noreply.github.com>
2023-03-10 19:31:28 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 # Clone the main branch and install Vism.
 
-. ./utils.sh
+perror() {
+	printf '\x1b[1;31m❌️ %s\x1b[m\n' "$1" >&2
+}
 
-check_binary "git"
+throw_error() {
+	perror "$1"
+	exit "$2"
+}
 
 if ! (git clone "git@github.com:qexat/PyVism.git" || git clone "https://github.com/qexat/PyVism.git"); then
 	throw_error "Could not fetch the repository" 1
 fi
+
+cd ./PyVism/ || throw_error "An unknown error occured" 1
 
 . ./install.sh
 
